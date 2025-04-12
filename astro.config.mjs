4dbaad4ae34e93c4ea595a44503bc8c8273f0c11
pbzweihander <pbzweihander@protonmail.com>
2025-04-12 23:31:42 +0900

    Update astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://pbzweihander.github.io",
+  base: "fediday.org",
   vite: {
     plugins: [tailwindcss()],
   },
